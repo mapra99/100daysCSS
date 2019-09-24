e587c7aff198ada36358719082548055f3012f74
Miguel Prada <mapra99@gmail.com>
2019-09-24 01:33:36 -0500

    Extended js function to handle the return animations

--- 001_dropdownbtn/js/main.js
+++ 001_dropdownbtn/js/main.js
@@ -5,11 +5,21 @@ function addDissapear() {
   var botLine = document.getElementById("bot-line");
   var topLine = document.getElementById("top-line");
   if(iconState){
+    midLine.classList.remove("appear-on-click");
+    botLine.classList.remove("moveback-on-click");
+    topLine.classList.remove("moveback-on-click");
+
     midLine.classList.add("dissapear-on-click");
     botLine.classList.add("moveup-on-click");
     topLine.classList.add("movedown-on-click");
   } else {
+    midLine.classList.remove("dissapear-on-click");
+    botLine.classList.remove("moveup-on-click");
+    topLine.classList.remove("movedown-on-click");
 
+    midLine.classList.add("appear-on-click");
+    botLine.classList.add("moveback-on-click");
+    topLine.classList.add("moveback-on-click");
   }
   iconState = !iconState;
 }
