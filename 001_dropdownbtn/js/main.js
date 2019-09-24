d48ee70e9e529a8d62c770c10cdb6f68cde44380
Miguel Prada <mapra99@gmail.com>
2019-09-24 00:59:28 -0500

    Added move-up for bottom line

--- 001_dropdownbtn/js/main.js
+++ 001_dropdownbtn/js/main.js
@@ -1,4 +1,7 @@
 function addDissapear() {
   var midLine = document.getElementById("mid-line");
+  var botLine = document.getElementById("bot-line");
+  var topLine = document.getElementById("top-line");
   midLine.classList.add("dissapear-on-click")
+  botLine.classList.add("moveup-on-click")
 }
