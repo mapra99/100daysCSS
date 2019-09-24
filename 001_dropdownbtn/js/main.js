8057621cdebb6e85edf160d2db0c34a7eb103903
Miguel Prada <mapra99@gmail.com>
2019-09-24 01:16:46 -0500

    arranged js to include in the future the contrary animation

--- 001_dropdownbtn/js/main.js
+++ 001_dropdownbtn/js/main.js
@@ -1,8 +1,15 @@
+var iconState = true;
+
 function addDissapear() {
   var midLine = document.getElementById("mid-line");
   var botLine = document.getElementById("bot-line");
   var topLine = document.getElementById("top-line");
-  midLine.classList.add("dissapear-on-click");
-  botLine.classList.add("moveup-on-click");
-  topLine.classList.add("movedown-on-click");
+  if(iconState){
+    midLine.classList.add("dissapear-on-click");
+    botLine.classList.add("moveup-on-click");
+    topLine.classList.add("movedown-on-click");
+  } else {
+
+  }
+  iconState = !iconState;
 }
