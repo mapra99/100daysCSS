4fe06ffca679152a2f4146c235cfe015c3c5c494
Miguel Prada <mapra99@gmail.com>
2019-09-24 01:01:50 -0500

    Added animation for top line

--- 001_dropdownbtn/js/main.js
+++ 001_dropdownbtn/js/main.js
@@ -2,6 +2,7 @@ function addDissapear() {
   var midLine = document.getElementById("mid-line");
   var botLine = document.getElementById("bot-line");
   var topLine = document.getElementById("top-line");
-  midLine.classList.add("dissapear-on-click")
-  botLine.classList.add("moveup-on-click")
+  midLine.classList.add("dissapear-on-click");
+  botLine.classList.add("moveup-on-click");
+  topLine.classList.add("movedown-on-click");
 }
